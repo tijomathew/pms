b42f1e3d4e2857303e312f035cdf42f2ec255b80
febin <febinj@cufa.ie>
2015-03-06 16:03:04 +0530

    priest and parish ui changes

--- src/main/java/org/pms/helpers/GridGenerator.java
+++ src/main/java/org/pms/helpers/GridGenerator.java
@@ -10,9 +10,22 @@ public final class GridGenerator {
 
     public GridContainer createGridContainer(Integer total, Integer page, Integer record, List<? extends GridRow> rows) {
         GridContainer gridContainer = new GridContainer();
-        gridContainer.setTotal(total);
+        Double totalDisplayPagesCount;
+        try {
+            totalDisplayPagesCount = (Math.ceil(total.doubleValue() / record.doubleValue()));
+        } catch (ArithmeticException ex) {
+            totalDisplayPagesCount = 0d;
+        }
+        Integer totalPagesDisplayCount = totalDisplayPagesCount.intValue();
+        if (record != 0 && totalPagesDisplayCount == 0)
+            totalPagesDisplayCount = 1;
+
+
+        if (record == 0l)
+            page = 0;
+        gridContainer.setTotal(totalPagesDisplayCount);
         gridContainer.setPage(page);
-        gridContainer.setRecords(record);
+        gridContainer.setRecords(total);
         gridContainer.setRows(rows);
         return gridContainer;
     }
