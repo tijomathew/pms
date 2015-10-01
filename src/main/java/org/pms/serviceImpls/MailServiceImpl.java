787f595e86d932efed9e67c8c131d098f9d54dfb
tijo <tijopower888@gmail.com>
2015-10-01 16:05:47 +0530

    Adjusting Navigation Bar Height

--- src/main/java/org/pms/serviceImpls/MailServiceImpl.java
+++ src/main/java/org/pms/serviceImpls/MailServiceImpl.java
@@ -32,7 +32,7 @@ public class MailServiceImpl implements MailService {
         try {
             mailMsg.setTo(registeredUser.getEmail());
             mailMsg.setSubject("Welcome to Parish Management System (PMS) of Syromalabar Church in Ireland");
-            mailMsg.setText("Your family’s unique login details to the PMS are given below."+"\n\nClick on the link and enter your user credentials for gaining access to PMS \n\nURL:http://pms.heart4needy.com \n\nLogin Email: " + registeredUser.getEmail() + "\n\nPassword: " + registeredUser.getPassword() + "\n\nNote:\n1. Please keep your login credentials for future access. \n\n2. You can also access PMS from www.syromalabar.ie \n\nThanking you," + "\n\nRev. Fr. Jose Bharanikulangara");
+            mailMsg.setText("Your family’s unique login details to the PMS are given below." + "\n\nClick on the link and enter your user credentials for gaining access to PMS \n\nURL:http://pms.heart4needy.com \n\nLogin Email: " + registeredUser.getEmail() + "\n\nPassword: " + registeredUser.getPassword() + "\n\nNote:\n1. Please keep your login credentials for future access. \n\n2. You can also access PMS from www.syromalabar.ie \n\nThanking you," + "\n\nRev. Fr. Jose Bharanikulangara\nRev. Fr. Antony Cheeramvelil\n\nTechnical Contact: Pintu Jacob (0873214964), pintujacob@gmail.com");
             mailSender.send(mimeMessage);
         } catch (Exception e) {
             System.out.println("---Errorrr from Mail Sending---");
